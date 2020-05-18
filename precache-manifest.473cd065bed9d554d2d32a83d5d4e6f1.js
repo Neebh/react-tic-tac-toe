a292d3993ddc60edaf9e697c86465a4855c7e6c8
Neeraj Bhatia <neeraj@Neerajs-MacBook-Pro.local>
2020-05-17 23:42:06 -0400

    Updates

---
+++ precache-manifest.473cd065bed9d554d2d32a83d5d4e6f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0d8360a81631d57d6be13b7823564ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fca5dce303fac35641b0",
+    "url": "/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "a740ed8f3265a1da9ecc",
+    "url": "/static/js/2.af71590a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.af71590a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fca5dce303fac35641b0",
+    "url": "/static/js/main.030f8375.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  }
+]);
